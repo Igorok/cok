9b02d7b80b5d0e5ddaeb3fef2cc9aa994830f6c1
Igor Dubinin <igordubinin1@yandex.ru>
2014-12-19 00:43:46 +0300

    fixed double events from socket

--- modules/chat/routes/socket.js
+++ modules/chat/routes/socket.js
@@ -8,9 +8,9 @@ var dbHelper = require(__dirname + '/../helpers/db_helper.js');
 var userApi = require(__dirname + '/../api/user.js');
 
 
-
 module.exports = function (app, io) {
     io.on('connection', function (socket) {
+        console.log('connection')
         var emitError = function (err) {
             console.trace(err);
             socket.emit('err', err);
@@ -24,6 +24,8 @@ module.exports = function (app, io) {
         * join chat group
         */
         socket.on('join', function (_formGroup) {
+            socket.leaveAll();
+            console.log('join')
             if (_.isEmpty(_formGroup)) {
                 return emitError(404);
             }
@@ -34,19 +36,22 @@ module.exports = function (app, io) {
             async.waterfall([
                 function (cb) {
                     userApi.checkAuth (data, safe.sure(cb, function (_user, _params) {
-                        if (! _user || ! _params) {
+                        if (! _user || ! _params || ! _params.chatId) {
                             return cb(403);
                         }
                         cUser = _user;
-                        dbHelper.collection("chatgroups", safe.sure(cb, function  (chatgroups) {
-                            chatgroups.findOne({_id: BSON.ObjectID(_params.chatId.toString()), "users._id" : _user._id}, safe.sure(cb, function( _group) {
-                                if (! _group) {
-                                    return cb(403);
-                                }
-                                cGroup = _group;
-                                chatId = cGroup._id.toHexString();
-                                cb();
-                            }));
+                        chatId = _params.chatId.toString();
+                        cb();
+                    }));
+                },
+                function (cb) {
+                    dbHelper.collection("chatgroups", safe.sure(cb, function  (chatgroups) {
+                        chatgroups.findOne({_id: BSON.ObjectID(chatId), "users._id" : cUser._id}, safe.sure(cb, function( _group) {
+                            if (! _group) {
+                                return cb(403);
+                            }
+                            cGroup = _group;
+                            cb();
                         }));
                     }));
                 },
@@ -84,8 +89,7 @@ module.exports = function (app, io) {
                 if (err) {
                     return emitError(err);
                 } else {
-                    console.log('join ', chatId)
-                    socket.to(chatId).emit('join', {cUser: cUser, cGroup: cGroup});
+                    socket.broadcast.to(chatId).emit('join', {cUser: cUser, cGroup: cGroup});
                     socket.emit('join', {cUser: cUser, cGroup: cGroup});
                 }
             });
@@ -138,8 +142,7 @@ module.exports = function (app, io) {
                 if (err) {
                     return emitError(err);
                 } else {
-                    console.log('msg ' ,chatId)
-                    socket.to(chatId).emit('message', {
+                    socket.broadcast.to(chatId).emit('message', {
                         userId: cUser._id,
                         login: cUser.login,
                         chatId: chatId,
@@ -156,141 +159,32 @@ module.exports = function (app, io) {
                 }
             });
         });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // leave room
         socket.on('disconnect', function(){
             console.log('disconnect', chatId)
-            socket.leave(chatId);
+            socket.leaveAll();
         });
     });
 };
-/*
-var mongoose = require('mongoose');
-var async = require('async')
-var _ = require('lodash')
-var redis = require("redis");
-clientRedis = redis.createClient();
-//model require
-var UserModel = require(__dirname + '/../models/user.js');
-var ChatGroupModel = require(__dirname + '/../models/chatgroup.js');
-var ChatMessageModel = require(__dirname + '/../models/chatmessage.js');
-var UserHelper = require(__dirname + '/../helpers/userhelper.js');
-var user_helper = new UserHelper();
-module.exports = function (app, io) {
-    io.on('connection', function (socket)
-    {
-        var login = null;
-        var userId = null;
-        var chatRoom = null;
-        socket.on('join', function (room) {
-            clientRedis.hgetall(room.token.toString(), function (err, _user)
-            {
-                if (err) {
-                    console.log('1', err);
-                }
-                if (! _user) {
-                    socket.emit('join', false);
-                } else {
-                    userId = _user.id;
-                    login = _user.login;
-                    async.waterfall([
-                        function (cb) {
-                            ChatGroupModel.findOne({_id: room.group.toString(), 'users.id': userId}, cb);
-                        },
-                        function (group, cb) {
-                            if (group) {
-                                chatRoom = room.group.toString();
-                                var res = {
-                                    login: login,
-                                    time: new Date(),
-                                    data: "i,m online",
-                                    permission: true,
-                                }
-
-                                socket.join(chatRoom);
-                                socket.emit('join', userId);
-                                socket.broadcast.to(chatRoom).emit('join', userId);
-                                socket.emit('message', res);
-                                socket.broadcast.to(chatRoom).emit('message', res);
-                            } else {
-                                socket.emit('join', false);
-                            }
-                            cb();
-                        }
-                    ], function (err) {
-                        if (err) {
-                            console.log('2');
-                            console.log(err);
-                        }
-                    })
-                }
-            });
-        });
-
-        // message
-        socket.to(chatRoom).on('message', function (msg)
-        {
-            clientRedis.hgetall(msg.token.toString(), function (err, _user)
-            {
-                if (err) {
-                    console.log('3');
-                    console.log(err);
-                }
-                if (! _user) {
-                    var res = {
-                        userId: _user.id,
-                        login: login,
-                        time: new Date(),
-                        data: "sorry, it is not your chat",
-                        permission: false,
-                    }
-                    socket.emit('message', res);
-                } else {
-                    async.waterfall([
-                        function (cb) {
-                            var newMessage = new ChatMessageModel({
-                                groupId: chatRoom,
-                                senderId: userId,
-                                senderLogin: login,
-                                message: msg.message.toString(),
-                                date: new Date()
-                            });
-                            newMessage.save(function (err) {
-                                if (err) {
-                                    console.log('4');
-                                    console.log(err);
-                                }
-                                cb();
-                            });
-                        }
-                    ], function (err) {
-                        if (err) {
-                            console.log('5');
-                            console.log(err)
-                        }
-                        var res = {
-                            userId: _user.id,
-                            login: login,
-                            time: new Date(),
-                            data: msg.message.toString(),
-                            permission: true,
-                        }
-                        socket.emit('message', res);
-                        socket.broadcast.to(chatRoom).emit('message', res);
-                    });
-                }
-            });
-        });
-        // leave room
-        socket.on('disconnect', function(){
-            var res = {
-                login: login,
-                time: new Date(),
-                data: "i'm offline",
-                permission: true,
-            }
-            socket.broadcast.to(chatRoom).emit('message', res);
-            socket.leave(chatRoom);
-        });
-    });
-};
-*/
